155838604feca66ef004e2a92a712d2351c0cbce
Anna Sidorova <xbitstream@gmail.com>
2019-11-12 12:38:50 +0300

    ref

--- core/src/test/kotlin/Tests.kt
+++ core/src/test/kotlin/Tests.kt
@@ -3,7 +3,6 @@ import io.kotlintest.matchers.types.shouldBeInstanceOf
 import io.kotlintest.matchers.types.shouldBeNull
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.FunSpec
-import pro.horovodovodo4ka.kodable.core.Koder
 import pro.horovodovodo4ka.kodable.core.defaults.DoubleKodable
 import pro.horovodovodo4ka.kodable.core.json.JsonReader
 import pro.horovodovodo4ka.kodable.core.json.JsonWriter
@@ -11,7 +10,6 @@ import pro.horovodovodo4ka.kodable.core.json.arrayElement
 import pro.horovodovodo4ka.kodable.core.json.invoke
 import pro.horovodovodo4ka.kodable.core.json.isNextNull
 import pro.horovodovodo4ka.kodable.core.json.objectProperty
-import pro.horovodovodo4ka.kodable.core.types.KodablePath
 import pro.horovodovodo4ka.kodable.core.types.kodablePath
 import pro.horovodovodo4ka.kodable.core.utils.dekode
 import pro.horovodovodo4ka.kodable.core.utils.enkode
@@ -57,18 +55,18 @@ class SerializersTest : FunSpec({
 
     test("encoder: nesting encoding") {
         val str = TestKodable.enkode(test)
-        str.shouldBe("{\"a\":{\"aee\":1},\"ints\":[[1,2]],\"format\":\"ae!\",\"for\":null}")
+        str.shouldBe("""{"a":{"aee":1},"ints":[[1,2]],"format":"ae!","for":null}""")
     }
 
     test("encoder: external kodable") {
         val str = DependencyTestKodable.enkode(DependencyTest(Dependence(1)))
-        str.shouldBe("{\"dependency\":{\"some\":1}}")
+        str.shouldBe("""{"dependency":{"some":1}}""")
     }
 
     test("polymorphic encode") {
         val poly = listOf(P1(10), P2("yay!"))
         val str = PolySerializer.list.enkode(poly)
-        str.shouldBe("[{\"poly_type\":\"p1\",\"i\":10},{\"poly_type\":\"p2\",\"s\":\"yay!\"}]")
+        str.shouldBe("""[{"poly_type":"p1","i":10},{"poly_type":"p2","s":"yay!"}]""")
     }
 
     test("polymorphic decoder") {
@@ -99,7 +97,7 @@ class EncodingTests : FunSpec({
         val ret = JsonWriter {
             writeString(" \n\t\rð\"\u001F ")
         }
-        ret.shouldBe("\" \\n\\t\\rð\\\"\\u001F \"")
+        ret.shouldBe("""" \n\t\rð\"\u001F """")
     }
 
     test("number") {
@@ -136,7 +134,7 @@ class EncodingTests : FunSpec({
         val ret = JsonWriter {
             iterateObject(props)
         }
-        ret.shouldBe("{\"a\":1,\"b\":\"yay!\"}")
+        ret.shouldBe("""{"a":1,"b":"yay!"}""")
     }
 
     test("array") {
@@ -147,7 +145,7 @@ class EncodingTests : FunSpec({
         val ret = JsonWriter {
             iterateArray(elements)
         }
-        ret.shouldBe("[1,\"yay!\"]")
+        ret.shouldBe("""[1,"yay!"]""")
     }
 })
 
@@ -173,7 +171,7 @@ class DecodingTests : FunSpec({
     }
 
     test("object") {
-        JsonReader("{\"a\": 1, \"b\" : null, \"c\": 1}").iterateObject {
+        JsonReader("""{"a": 1, "b" : null, "c": 1}""").iterateObject {
             when (it) {
                 "a" -> {
                     readNumber().toInt().shouldBe(1)
@@ -200,7 +198,7 @@ class DecodingTests : FunSpec({
     }
 
     test("mixed") {
-        JsonReader("[{\"a\":1, \"d\": \"a\"}, {\"b\":2, \"c\": null}]")
+        JsonReader("""[{"a":1, "d": "a"}, {"b":2, "c": null}]""")
             .iterateArray {
                 iterateObject {
                     when (it) {
@@ -219,7 +217,7 @@ class DecodingTests : FunSpec({
     }
 
     test("recorded") {
-        val reader = JsonReader("[{\"type\": \"int\", \"value\": 1}, {\"type\": \"string\", \"value\": \"aaa\"}]")
+        val reader = JsonReader("""[{"type": "int", "value": 1}, {"type": "string", "value": "aaa"}]""")
 
         reader.iterateArray {
             var type = ""
@@ -243,103 +241,5 @@ class DecodingTests : FunSpec({
                 }
             }
         }
-    }
-})
-
-@Koder
-data class SocialProvider(val id: Int, val name: String)
-
-class HardBass : FunSpec({
-    test("with urls") {
-        val json = """
-{
-    "meta": [],
-    "data": [
-        {
-            "id": 15,
-            "name": "Password",
-            "isActive": true,
-            "authData": []
-        },
-        {
-            "id": 16,
-            "name": "VK",
-            "isActive": true,
-            "oauthUrl": "http://oauth.vk.com/authorize?client_id=7154623&scope=email&response_type=code&redirect_uri=https%3A%2F%2Fweb-stage.dev.more.tv%2Foauth%2Fcallback%2Fvk&state=16",
-            "redirectUrl": "https://web-stage.dev.more.tv/oauth/callback/vk",
-            "authData": {
-                "scope": "email",
-                "client_id": "7154623",
-                "client_secret": "aMOEMJsOnurmMWQAs1MN"
-            }
-        },
-        {
-            "id": 17,
-            "name": "Facebook",
-            "isActive": true,
-            "oauthUrl": "https://www.facebook.com/dialog/oauth?client_id=696017504207217&scope=email&response_type=code&redirect_uri=https%3A%2F%2Fweb-stage.dev.more.tv%2Foauth%2Fcallback%2Ffacebook&state=17",
-            "redirectUrl": "https://web-stage.dev.more.tv/oauth/callback/facebook",
-            "authData": {
-                "scope": "email",
-                "client_id": "696017504207217",
-                "data_fields": "name,email",
-                "client_secret": "9faa9638691fcf4cd47de2107dd29182"
-            }
-        },
-        {
-            "id": 18,
-            "name": "OK",
-            "isActive": true,
-            "oauthUrl": "https://connect.ok.ru/oauth/authorize?client_id=512000052419&response_type=code&redirect_uri=https%3A%2F%2Fweb-stage.dev.more.tv%2Foauth%2Fcallback%2Fok&state=18&scope=GET_EMAIL",
-            "redirectUrl": "https://web-stage.dev.more.tv/oauth/callback/ok",
-            "authData": {
-                "scope": "GET_EMAIL",
-                "app_id": "512000052419",
-                "client_public": "CEMNMFJGDIHBABABA",
-                "client_secret": "B9574C02C4A752761DE76820"
-            }
-        },
-        {
-            "id": 19,
-            "name": "PIN",
-            "isActive": true,
-            "authData": []
-        },
-        {
-            "id": 20,
-            "name": "VK_Mobile",
-            "isActive": true,
-            "authData": []
-        },
-        {
-            "id": 21,
-            "name": "Facebook_Mobile",
-            "isActive": true,
-            "authData": []
-        },
-        {
-            "id": 22,
-            "name": "OK_Mobile",
-            "isActive": true,
-            "authData": []
-        },
-        {
-            "id": 23,
-            "name": "DeviceId",
-            "isActive": true,
-            "authData": []
-        },
-        {
-            "id": 24,
-            "name": "PIN",
-            "isActive": true,
-            "authData": []
-        }
-    ]
-}
-""".trimIndent()
-
-        val resp = SocialProviderKodable.list.dekode(json, KodablePath(".data"))
-        print(resp)
     }
 })
